4c1d0696158f012fcd86df8d3e467674518127c0
UH <554c46@gmail.com>
2021-04-27 19:51:02 +0200

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 
 setup(name='threesplit',
-      version='0.1.0',
+      version='0.1.1',
       description=(
           "Three-way data split into training set, "
           "validation set, and test set."),
